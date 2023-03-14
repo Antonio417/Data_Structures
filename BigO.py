a87f69071219f9a2a273b9bd2b5987d2b24b8b0a
Antonio <airantoniofernando@gmail.com>
2023-03-15 07:46:42 +1100

    Added more Example for O(n^2)

--- BigO.py
+++ BigO.py
@@ -25,3 +25,13 @@ def print_items(n):
 # print_items(10)
 
 # This is O(n^2) since we have 100 output with 10 input value
+
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                print(i,j,k)
+
+print_items(10)
+
+# This is still O(N^2), it does not matter if it is n^3 or n^4 we just simplify it as O(N^2)
