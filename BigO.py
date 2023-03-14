1a236a0c1b03cd47266a974c63e84abd6989df7a
Antonio <airantoniofernando@gmail.com>
2023-03-15 07:40:40 +1100

    Added O(n) Example

--- BigO.py
+++ BigO.py
@@ -5,3 +5,14 @@ def print_items(n):
 print_items(10)
 
 # This is O(n) since it is proportional
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+    
+    for j in range(n):
+        print(j)
+
+print_items(10)
+
+# This is still O(n) because of dropping the constant.
