4b57d4178d47e4825938f86232be8f92bd155904
Antonio <airantoniofernando@gmail.com>
2023-03-17 20:11:06 +1100

    Added Print Statements

--- Pointers.py
+++ Pointers.py
@@ -1,3 +1,19 @@
 # Pointers in Python
 num1 = 11
 num1 = num1
+
+print("Before num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2)) 
+
+num2 = 22 
+
+print("\nAfter num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2) 
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2))
