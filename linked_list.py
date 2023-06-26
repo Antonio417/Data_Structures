3c84f875610a9dcda8081cb6241e94713667aed2
Antonio <airantoniofernando@gmail.com>
2023-06-26 23:09:32 +1000

    pop linked list

--- linked_list.py
+++ linked_list.py
@@ -30,17 +30,22 @@ class LinkedList:
         return True
     
     def pop(self):
-        if self.tail.next == None:
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
             self.head = None
             self.tail = None
-        else:
-            temp = self.head
-            pre = self.head
-            while(temp.next):
-                pre = temp
-                temp = temp.next
-            self.tail = pre
-            self.tail.next = None
+        return temp
+
 
 class Node:
     def __init__(self, value):
