01bce3c0bbc5b283e84aa8f6b1617f9f9ac764e4
Antonio <airantoniofernando@gmail.com>
2023-06-26 21:08:35 +1000

    Reviewing Linked List

--- linked_list.py
+++ linked_list.py
@@ -5,3 +5,17 @@
 # Prepend Linked List is O(1)
 # Pop first Linked List is O(1)
 # Insert, remove, lookup by index and lookup by value is O(n)
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+my_linked_list = LinkedList(20)
+print(my_linked_list)
